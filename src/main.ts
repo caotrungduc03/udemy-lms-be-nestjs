7961688510e1d3ca30ce13df1110865237b320ed
caotrungduc <trungdc2003@gmail.com>
2024-07-18 13:55:55 +0700

    feat: handle global exception and custom response

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './utils/allExceptionsFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const httpAdapterHost: HttpAdapterHost = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
+
   await app.listen(4000);
 }
 bootstrap();
---
+++ src/utils/allExceptionsFilter.ts
@@ -0,0 +1,38 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { CustomResponse } from './customResponse';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    let statusCode: number = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message: string = 'Internal Server Error';
+    if (exception instanceof HttpException) {
+      statusCode = exception.getStatus();
+      if (typeof exception.getResponse() === 'object') {
+        message = exception.getResponse()['message'];
+      } else {
+        message = exception.getResponse().toString();
+      }
+    } else if (exception instanceof Error) {
+      message = exception.message;
+    }
+
+    const responseBody: CustomResponse = new CustomResponse(
+      statusCode,
+      message,
+      null,
+      httpAdapter.getRequestUrl(request),
+    );
+
+    httpAdapter.reply(response, responseBody, statusCode);
+  }
+}
